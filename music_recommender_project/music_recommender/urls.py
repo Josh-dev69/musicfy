3fd6791449252743a7ba6ae16bce149cf8fb1307
Josh-dev69 <ebubejoshua69@gmail.com>
2024-03-23 07:02:58 +0100

    Code Update

--- music_recommender_project/music_recommender/urls.py
+++ music_recommender_project/music_recommender/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="music_recommender-home"),
-    path('about/', views.about, name="music_recommender-about"),
+    path('', views.home, name="musicify-home"),
+    path('about/', views.about, name="musicify-about"),
 ]
--- music_recommender_project/music_recommender/views.py
+++ music_recommender_project/music_recommender/views.py
@@ -1,30 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-posts = [
-    {
-        'author' : 'Ebube Joshua',
-        'title' : 'Blog Post 1',
-        'content' : 'First Post content',
-        'date_posted' : 'March 16, 2024'
-    },
-    
-    {
-        'author' : 'Ebube Joshua',
-        'title' : 'Blog Post 2',
-        'content' : 'Second Post content',
-        'date_posted' : 'March 16, 2024'
-    }
-]
-
-
-context = {
-    'posts': posts
-}
-
 # Create your views here.
 def home(request):
-    return render(request, 'main/index.html', context)
+    return render(request, 'main/index.html')
 
 def about(request):
     return render(request, 'main/about.html', { 'title': 'About' })
