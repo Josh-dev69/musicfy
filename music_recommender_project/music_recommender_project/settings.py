e1c8fef51806a782b2faaa9e5b6e1776b6468bcb
Josh-dev69 <ebubejoshua69@gmail.com>
2024-03-24 09:50:23 +0100

    Crispy Form

--- music_recommender_project/music_recommender_project/settings.py
+++ music_recommender_project/music_recommender_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'music_recommender.apps.MusicRecommenderConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
