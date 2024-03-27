0a1bf943255f92174be3620adebec90cdcf4df95
Josh-dev69 <ebubejoshua69@gmail.com>
2024-03-27 22:24:11 +0100

    Update

--- music_recommender_project/users/forms.py
+++ music_recommender_project/users/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import login, authenticate
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -9,3 +9,9 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label="Username or Email")
+
+    class Meta:
+        fields = ["username", "password"]
--- music_recommender_project/users/views.py
+++ music_recommender_project/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import login, authenticate
-from .forms import UserRegisterForm
+from django.contrib.auth import login, authenticate, logout
+from .forms import UserRegisterForm, UserLoginForm
 
 # Create your views here.
 def register(request):
@@ -15,3 +15,25 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form, 'title': 'About'})
+
+# Login View
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('musicify-home')  # Redirect to home page after login
+            else:
+                messages.error(request, 'Invalid username or password.')
+    else:
+        form = UserLoginForm()
+    return render(request, 'users/login.html', {'form': form, 'title': 'Login'})
+
+# Logout View
+def user_logout(request):
+    logout(request)
+    return redirect('login')  # Redirect to login page after logout
