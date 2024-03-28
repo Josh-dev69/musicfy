593be4e61ab0df02c485170bd11d7447d23a0164
Josh-dev69 <ebubejoshua69@gmail.com>
2024-03-28 23:15:07 +0100

    Login

--- music_recommender_project/users/views.py
+++ music_recommender_project/users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account for {username} has been created!')
-            return redirect('users-login')
+        return redirect('users-login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form, 'title': 'About'})
